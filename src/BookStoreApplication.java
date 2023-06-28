e707c721d432e2e385c7c3c672285da4a806737f
Geree <smithzero94@gmail.com>
2023-06-28 19:18:37 +0200

    1. task implemented

--- src/BookStoreApplication.java
+++ src/BookStoreApplication.java
@@ -1,5 +1,21 @@
 public class BookStoreApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Bookstore bookstore = new Bookstore();
+
+        bookstore.addBook(new Book("abc", "someOne", 10));
+        bookstore.addBook(new Book("def", "someTwo", 99.9));
+        bookstore.addBook(new Book("ghi", "someThree", 15.9));
+        bookstore.addBook(new Book("jkl", "someFour", 1));
+        bookstore.addBook(new Book("mno", "someFive", 70));
+        bookstore.addBook(new Book("pqr", "someSix", 77));
+
+
+        bookstore.displayBooks();
+
+        System.out.println();
+
+        System.out.println(bookstore.findBook("bbc"));
+
+
     }
 }
--- src/Bookstore.java
+++ src/Bookstore.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bookstore {
+    List<Book> books = new ArrayList<>();
 
+    public void addBook(Book book) {
+        books.add(book);
+    }
 
+    public Book findBook(String title) throws BookNotFoundException {
+        try {
+            for (int i = 0; i < books.size(); i++) {
+                if (title.equalsIgnoreCase(books.get(i).getTitle())) {
+                    return books.get(i);
+                } else {
+                    throw new BookNotFoundException();
+                }
+            }
+        } catch (BookNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public void displayBooks() {
+        for (var actual : books) {
+            System.out.println(actual);
+        }
+    }
 }
